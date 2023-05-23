62a246e7a2a0be8937701e1b41e9247644ddc40a
Dima B <belozerov.msk@gmail.com>
2023-05-23 14:00:25 +0300

    GUI is done

---
+++ GUI_FoxTailVPN/include/countries.h
@@ -0,0 +1,37 @@
+#ifndef COUNTRIES_H
+#define COUNTRIES_H
+
+#include <QDialog>
+
+namespace Ui {
+class Countries;
+}
+
+class Countries : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Countries(QWidget *parent = nullptr);
+    ~Countries();
+
+private:
+    Ui::Countries *ui;
+
+    bool buttonCountryClicked;
+    std::string defaultConfiguration;
+
+signals:
+    void backMainWindow();
+    void valueChangedButtonCountryClicked(bool value);
+    void valueChangedDefaultConfiguration(std::string value);
+
+
+private slots:
+    void back();
+    void readLEServerIP();
+    void deleteServerIP();
+    void chooseServerIP();
+};
+
+#endif // COUNTRIES_H
